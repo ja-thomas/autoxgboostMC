51d100b70c086845b15582e59c9ecbb85bb4a17e
pfistfl <pfistererf@googlemail.com>
2019-04-14 23:23:13 +0200

    Autoxgbmc

--- R/measures.R
+++ R/measures.R
@@ -1,24 +1,35 @@
 #' Squared difference in F1 Scores between groups
 #' @export
 fairf1 = mlr::makeMeasure(id = "fairness.f1", minimize = TRUE, properties = c("classif", "response"),
-  extra.args = list(),
-  fun = function(task, model, pred, feat, extra.args) {
-    # group is a logical vector indicating group1 or group2
-    g1 = pred$data[extra.args$extra.args$group, ]
-    g2 = pred$data[!extra.args$extra.args$group, ]
-    g1f1 = measureF1(g1$truth, g1$response, pred$task.desc$positive)
-    g2f1 = measureF1(g2$truth, g2$response, pred$task.desc$positive)
-    (g1f1 - g2f1)^2
+  extra.args = list(groupvar = NULL), best = 0, worst = 1,
+  fun = function(task, model, pred, feats, extra.args) {
+    fs = aggregate(as.formula(paste0("truth + response ~ ", extra.args$var)), pred$data, 
+      function(truth, response) {
+        measureF1(truth, response, pred$task.desc$positive)
+    })
+    (sum(fs - mean(fs)))^2
+  }
+)
+
+#' Squared difference in False Postive rate between groups
+#' @export
+fairfpr = mlr::makeMeasure(id = "fairness.f1", minimize = TRUE, properties = c("classif", "response"),
+  extra.args = list(groupvar = NULL), best = 0, worst = 1,
+  fun = function(task, model, pred, feats, extra.args) {
+    fs = aggregate(as.formula(paste0("truth + response ~ ", extra.args$groupvar)), pred$data, 
+      function(truth, response) {
+        measureFPR(truth, response, pred$task.desc$positive)
+    })
+    (sum(fs - mean(fs)))^2
   }
 )
 
 
-
-#' Sparsity as percentage of available features
+#' Sparsity as percentage of used features
 #' @export
 xgb.sparsity = mlr::makeMeasure(id = "sparse.xgb", minimize = TRUE, properties = c("classif", "response"),
-  extra.args = list(),
-  fun = function(task, model, pred, extra.args) {
+  extra.args = list(), best = 0, worst = 1L,
+  fun = function(task, model, pred, feats, extra.args) {
     if(model$learner$package != "xgboost") stop("Measure sparse.xgb is only available for xgboost models")
     nfeats = setdiff(unique(xgboost::xgb.model.dt.tree(model = model$learner.model)$Feature), "Leaf")
     nfeats / sum(model$task.desc$n.feat)
